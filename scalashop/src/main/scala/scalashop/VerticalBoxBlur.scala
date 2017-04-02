d9c574f7984fedf3b9c34ea4beaafcf8d1cf0cf3
Konrad Waniek <konrad.waniek@gmail.com>
2017-04-02 21:03:12 +0200

    10/10

---
+++ scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,83 @@
+package scalashop
+
+import common._
+import org.scalameter._
+
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig: MeasureBuilder[Unit, Double] = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer new Warmer.Default
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+    * `dst`, starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each column, `blur` traverses the pixels by going from top to
+    * bottom.
+    */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for (x <- from until end) {
+      for (y <- 0 until dst.height) {
+        dst.update(x, y, boxBlurKernel(src, x, y, radius))
+      }
+    }
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * columns.
+    */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+
+    def processSingleColumn(): Unit = {
+      (0 until src.width)
+        .map(region => task(blur(src, dst, region, region + 1, radius)))
+        .foreach(_.join())
+    }
+
+    def processMultipleColumns(sectionSize: Int): Unit = {
+      (0 to src.width)
+        .sliding(sectionSize, sectionSize - 1)
+        .map(sec => task(blur(src, dst, sec.head, sec.last, radius)))
+        .foreach(_.join())
+    }
+
+    val sectionSize = src.width / numTasks + 1
+
+    sectionSize match {
+      case 1 => processSingleColumn()
+      case _ => processMultipleColumns(sectionSize)
+    }
+  }
+
+}
